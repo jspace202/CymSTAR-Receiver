27f2742ba87910d44d4a2fa5989e13a55af841b4
Jaden Patel <jspace202@gmail.com>
2023-11-30 15:33:17 -0600

    cycle between 0 and 359

--- Simple Server/server.js
+++ Simple Server/server.js
@@ -3,13 +3,16 @@ const WebSocket = require('ws');
 const server = new WebSocket.Server({ port: 3000, host: '192.168.1.2' });
 
 server.on('connection', (socket) => {
-  // Set an interval to send changing float numbers
-  const interval = setInterval(() => {
-    // Generate a random float number between 0 and 359.99
-    const floatNumber = Math.random() * 360;
+  // Initialize the current float number
+  let currentFloatNumber = 0;
 
-    // Send the float number to the connected client
-    socket.send(floatNumber.toString());
+  // Set an interval to continuously increase and decrease the float number by 0.01
+  const interval = setInterval(() => {
+    // Increase or decrease the float number by 0.01
+    currentFloatNumber = (currentFloatNumber + 0.01) % 360;
+
+    // Send the float number to the connected client, rounded to two digits
+    socket.send(currentFloatNumber.toFixed(2).toString());
   }, 1000); // Adjust the interval as needed (here, it sends a new number every second)
 
   // Handle messages from the client (if needed)
